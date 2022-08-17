78d6c7d33c45351af1d3a6cd0e39cdedbeffe38d
MB7232SP <mauryaarvind962@gmail.com>
2022-08-17 12:07:18 +0530

    no data

--- db.js
+++ db.js
@@ -2,13 +2,29 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
-const reliance = require("./data/reliance_data");
+const appliances = require("./data/appliances");
+const laptop = require("./data/laptop");
+const roll1 = require("./data/roll1");
+const roll2 = require("./data/roll2");
+const soundbar = require("./data/soundbar");
+const television = require("./data/television");
+const tvForHomePage = require("./data/tvForHomePage");
+const watches = require("./data/watches");
+const profile = require("./data/profile");
 module.exports = {
   items,
   tags,
   brands,
   itemTypes,
-  reliance
+  appliances,
+  laptop,
+  roll1,
+  roll2,
+  soundbar,
+  television,
+  watches,
+  tvForHomePage,
+  profile
 }
 
 
